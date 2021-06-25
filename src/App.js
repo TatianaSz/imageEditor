1f10ee7b4ff88fd03fcf513eafcc8686f23866bc
Tatiana <Tatiszulik777@gmail.com>
2021-06-25 21:46:37 +0200

    added normalize

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react';
 import Menu from "./Menu"
 import Options from "./Options"
 import ImageUpload from './ImageUpload';
+import "./../node_modules/normalize.css/normalize.css"
 import "./css/app.css";
+
 
 
 
--- src/Menu.js
+++ src/Menu.js
@@ -1,16 +1,20 @@
 import React from "react"
 import "./css/menu.css"
-import { FaBeer } from 'react-icons/fa';
+import { BsCardImage } from 'react-icons/bs';
+import {HiOutlineAdjustments} from 'react-icons/hi'
+import {IoResize} from 'react-icons/io5'
+import {IoText} from "react-icons/io5"
+import {IoShapesOutline} from "react-icons/io5"
 
 function Menu(){
     return (
         <div className="menu">
             <ul>
-                <li> <FaBeer /> </li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                <li><BsCardImage /> </li>
+                <li><HiOutlineAdjustments /></li>
+                <li><IoResize /></li>
+                <li><IoText /></li>
+                <li><IoShapesOutline /></li>
             </ul>
 
 
