8a3822225ed594296433c62baab4930009c68620
Tatiana <Tatiszulik777@gmail.com>
2021-06-26 16:17:06 +0200

    styled uploading button

--- src/Options.js
+++ src/Options.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./css/options.css"
+import {BsFilePlus} from "react-icons/bs"
 
 function Options(props){
     return (
         <div className="options" >
- <input type="file" onChange={props.onChange}/>
-       
-
+        <label className="uploader">
+            <div className="centered">
+                Add image <br/>
+                <BsFilePlus className="uploadIcon"/>
+            </div>
+        <input  type="file" onChange={props.onChange}/>
+        </label>
+        
         </div>
     )
 }
