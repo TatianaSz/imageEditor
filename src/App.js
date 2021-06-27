d0ad0bc0c17a8439126cc3f7585a9d0a2217c327
Tatiana <Tatiszulik777@gmail.com>
2021-06-27 09:44:10 +0200

    moved functions to app component and uptaded refs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Menu from "./Menu"
 import Options from "./Options"
 import ImageUpload from './ImageUpload';
@@ -9,16 +9,17 @@ import "./css/app.css";
 
 
 function App() {
+  const canva = useRef(null);
+  const image = useRef(null);
+  
   const [file, setFile] = useState(null);
-  
-  
   const [filtr, setFiltr] = useState(100);
+
   function addValue(e){
   setFiltr(e.target.value)
   console.log(filtr)
   }
  
-  
  function addFile(e){
   URL.revokeObjectURL(file)
    setFile(
@@ -26,14 +27,26 @@ function App() {
     )
  
   }
+
+  useEffect(() => {   
+    const canvas = canva.current;
+    const ctx = canvas.getContext("2d")
+    
+    const img = image.current;
+    img.onload=()=>{
+     var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
+     var x = (canvas.width / 2) - (img.width / 2) * scale;
+ var y = (canvas.height / 2) - (img.height / 2) * scale;
+ ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+     
+    }
+ });
   
-  
-   
     return (
       <div className="app">
         <Menu clicked="menu-clicked"/>
         <Options onChange={addFile} slider={addValue}/>
-        <ImageUpload src={file} val={filtr}/>
+        <ImageUpload canvaRef={canva} imageRef={image} src={file} val={filtr}/>
       </div>
     );
   
--- src/ImageUpload.js
+++ src/ImageUpload.js
@@ -2,31 +2,12 @@ import React, { useEffect, useRef } from 'react';
 import "./css/image.css"
 
 function ImageUpload(props){
-   const filterCss={filter: `brightness(${props.val}%)` }
-
-   useEffect(() => {   
-       const canvas = canva.current;
-       const ctx = canvas.getContext("2d")
-       const img = image.current;
-       img.onload=()=>{
-        var scale = Math.min(canvas.width / img.width, canvas.height / img.height);
-        var x = (canvas.width / 2) - (img.width / 2) * scale;
-    var y = (canvas.height / 2) - (img.height / 2) * scale;
-    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
-        
-       }
-       
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    });
-
-    const canva = useRef(null);
-    const image = useRef(null);
-
+  
     return (
 <div className="image" >
     <div className="image-max">
-    <canvas ref={canva} width={640} height={425} />
-    <img  ref={image} id="myImg" src={props.src} style={filterCss}/>
+    <canvas ref={props.canvaRef} width={640} height={425} />
+    <img  ref={props.imageRef} id="myImg" src={props.src} />
     </div>
 </div>
     )
