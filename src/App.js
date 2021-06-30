d9b8aa7f95209d5b35eceef1b8d3ef55781c2cb4
Tatiana <Tatiszulik777@gmail.com>
2021-06-30 16:27:58 +0200

    implemented working rgb to hsl converter from https://gist.github.com/mjackson/5311256

--- src/App.js
+++ src/App.js
@@ -38,6 +38,53 @@ function App() {
     }
  });
  
+ function rgbToHsl(r, g, b){
+  r /= 255, g /= 255, b /= 255;
+  var max = Math.max(r, g, b), min = Math.min(r, g, b);
+  var h, s, l = (max + min) / 2;
+
+  if(max == min){
+      h = s = 0; // achromatic
+  }else{
+      var d = max - min;
+      s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+      switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+      }
+      h /= 6;
+  }
+
+  return [h, s, l];
+}
+function hslToRgb(h, s, l){
+  var r, g, b;
+
+  if(s == 0){
+      r = g = b = l; // achromatic
+  }else{
+      var hue2rgb = function hue2rgb(p, q, t){
+          if(t < 0) t += 1;
+          if(t > 1) t -= 1;
+          if(t < 1/6) return p + (q - p) * 6 * t;
+          if(t < 1/2) return q;
+          if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+          return p;
+      }
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1/3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1/3);
+  }
+
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
+}
+
+
+
   function setBrightness(){
     const canvas = canva.current;
     const img = image.current;
@@ -53,6 +100,20 @@ function App() {
    
   var iD = canvas.getContext('2d').getImageData(0, 0, img.width, img.height);
   var dA = iD.data; 
+  //filters
+
+// for(var i = 0; i < dA.length; i += 4){
+//   var red=  dA[i]                       //red
+//   var green= dA[i+1]                     //green
+//    var blue = dA[i+2]                     //blue
+//   var hsl = rgbToHsl(red, green, blue)
+//   hsl[2]+=val/100
+//    var rgb = hslToRgb.apply(this, hsl)
+//   dA[i] = rgb[0]
+//   dA[i+1] = rgb[1]
+//   dA[i+2] = rgb[2]
+// }
+
 
    for(var i = 0; i < dA.length; i += 4)
      {
@@ -61,13 +122,13 @@ function App() {
      dA[i+2] += 255 * (val / 100);
   }
   
-  var factor = (259.0 * (cont + 255.0)) / (255.0 * (259.0 - cont));
+  // var factor = (259.0 * (cont + 255.0)) / (255.0 * (259.0 - cont));
 
-  for (var i = 0; i < dA.length; i+= 4) {
-    dA[i] = (factor * (dA[i] - 128.0) + 128.0);
-    dA[i+1] = (factor * (dA[i+1] - 128.0) + 128.0);
-    dA[i+2] = (factor * (dA[i+2] - 128.0) + 128.0);
-  }
+  // for (var i = 0; i < dA.length; i+= 4) {
+  //   dA[i] = (factor * (dA[i] - 128.0) + 128.0);
+  //   dA[i+1] = (factor * (dA[i+1] - 128.0) + 128.0);
+  //   dA[i+2] = (factor * (dA[i+2] - 128.0) + 128.0);
+  // }
 
   canvas.getContext('2d').putImageData(iD, 0, 0);
   
