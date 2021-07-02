9ed647b54526a570b43bf34009d094c118009b43
Tatiana <Tatiszulik777@gmail.com>
2021-07-02 14:46:44 +0200

    resizable div

--- src/CropDrag.js
+++ src/CropDrag.js
@@ -3,27 +3,18 @@ import Draggable from "react-draggable";
 import './css/drag.css'
 
 function CropDrag(props){
-const red = useRef(null)
 
     function Resize(e) {
-        let first=e.target.parentNode.firstChild
-      // console.log(e.target.parentNode.firstChild)
-     first.style.width =200 + "px"
-     //  e.target.style.width = (e.target.clientX -  e.target.offsetLeft) + 'px';
-      // e.target.style.height = (e.target.clientY -  e.target.offsetTop) + 'px';
-      
+        let w = document.getElementById("iddd")
+         w.style.width = (e.clientX -  w.getBoundingClientRect().left) + 'px';
+        w.style.height = (e.clientY -  w.getBoundingClientRect().top) + 'px';
       }
 return(
-    <div className="test">
-        <Draggable>
-            <div>
-                <Draggable  bounds=".image-max"><div ref={red} className="aaaa"></div></Draggable>
-                <Draggable onDrag={Resize}  bounds=".aaaa" ><div className="hand q"></div></Draggable>
-                <Draggable onDrag={Resize} bounds=".aaaa"><div className="hand w"></div></Draggable>
-                <Draggable onDrag={Resize} bounds=".aaaa"><div className="hand e"></div></Draggable>
-                <Draggable  onDrag={Resize}  bounds=".aaaa"><div className="hand r"></div></Draggable>
-            </div>
-        </Draggable>
+    <div id="idd" className="test">
+                <div  id="iddd" className="aaaa"></div>
+                <Draggable  bounds=".test" onDrag={Resize} onMouseMove={Resize} ><div className="hand r"></div></Draggable>
+          
+       
     </div>
   
 )
